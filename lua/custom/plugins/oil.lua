6f163e854603ba36f469711ff2a1b8fa5a2a5bd5
Till.Preikschat <till.preikschat@smart-cjm.com>
2025-02-13 03:20:13 +0100

    adding telescope oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,4 +8,21 @@ return {
     },
     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
   },
+
+  {
+    'albenisolmos/telescope-oil.nvim',
+    dependencies = {
+      {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+      },
+    },
+    config = function()
+      require('telescope').load_extension 'oil'
+
+      vim.keymap.set('n', '<leader>sF', '<cmd>Telescope oil<cr>', {
+        desc = '[F]olders',
+      })
+    end,
+  },
 }
