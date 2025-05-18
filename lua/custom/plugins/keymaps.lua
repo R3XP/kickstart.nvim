dc2eab2b45d3733b0196a70032db9023b47becd0
Till.Preikschat <till.preikschat@smart-cjm.com>
2025-05-18 22:07:07 +0200

    adding a few keymaps

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,3 +1,7 @@
 vim.keymap.set('n', '-', '<cmd>Oil<CR>')
 
+vim.keymap.set('n', '<C-q>', '<C-w>q', { desc = 'Close the current window' })
+
+vim.keymap.set('n', '<leader>bs', '<cmd>w<CR>', { desc = '[S]ave contens of current [b]uffer' })
+
 return {}
